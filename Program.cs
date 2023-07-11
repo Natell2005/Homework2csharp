c3ae431ef56ca6e478cc4cb00c6522fe39c4081f
Natalia Khasanova <natell_2005@mail.ru>
2023-07-11 17:49:19 +0300

    Решила задачу № 15

--- Program.cs
+++ Program.cs
@@ -13,29 +13,54 @@
 // else {
 //    Console.WriteLine($"Введенное число {number} - не является трехзначным"); 
 // }
+
+
 // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-Console.Write("Введите  число от 0 до 9999: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int position3 = (number/10%10);
-int position4 = (number%10);
+// Console.Write("Введите  число от 0 до 9999: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int position3 = (number/10%10);
+// int position4 = (number%10);
 
-if (number > 99 && number < 1000)
-{
-Console.WriteLine($"Третья цифра в числе {number} - это {position4}");
-} 
-if (number < 100)
-{
-   Console.WriteLine($"У введенного числа {number} - нет третьей цифры"); 
-}
-if (number > 999 && number < 9999)
-{
-Console.WriteLine($"Третья цифра в числе {number} - это {position3}");
-}
-else 
-{ 
-    Console.WriteLine($"Введенное число {number}  -  вне заданного диапазона");
-}
+// if (number > 99 && number < 1000)
+// {
+// Console.WriteLine($"Третья цифра в числе {number} - это {position4}");
+// } 
+// if (number < 100)
+// {
+//    Console.WriteLine($"У введенного числа {number} - нет третьей цифры"); 
+// }
+// if (number > 999 && number < 9999)
+// {
+// Console.WriteLine($"Третья цифра в числе {number} - это {position3}");
+// }
+// else 
+// { 
+//     Console.WriteLine($"Введенное число {number}  -  вне заданного диапазона");
+// }
+
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+    Console.Write("Введите номер дня недели: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+
+        if (number > 5 && number < 8 )
+        {
+    Console.WriteLine($"Да, день недели {number} - является выходным");
+    }
+     if (number > 0 && number < 6)
+    {
+    Console.WriteLine($"Нет, день недели {number}  - не является выходным"); 
+    }
+  if (number < 1 && number < 7)
+    {
+    Console.WriteLine("Вы ввели некорректное число!"); 
+    }
